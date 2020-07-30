efcd2674cafd04926c77b0cdc4908212de8a4f8f
safiur123 <34005613+safiur123@users.noreply.github.com>
2020-07-30 11:52:18 +0530

    add code

--- Find count of maximum consecutive 1s in a binary array.cpp
+++ Find count of maximum consecutive 1s in a binary array.cpp
@@ -1,1 +1,30 @@
+//created by Safiur Rahaman
+// date 30.7.20
+#include<bits/stdc++.h>
 
+using namespace std;
+void solved(int a[],int b)
+{
+    int res=0;
+    int count=1;
+    for(int i=1; i<b; i++)
+    {
+        if(a[i]!=a[i-1])
+        {
+            res=max(res,count);
+            count=1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    
+    cout<<res;
+}
+int main()
+{
+    int a[]={1,1,0,1,1,1,0};
+    int b=sizeof(a)/sizeof(a[0]);
+    solved(a,b);
+}
